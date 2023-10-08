8e94e2542025db8bba25c93b7406a7035dff8983
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-10-08 22:58:51 +0300

    Add page container and change data structure

--- app/(pages)/room/[id]/page.tsx
+++ app/(pages)/room/[id]/page.tsx
@@ -1,13 +1,16 @@
 "use client";
-import { doc, DocumentReference, DocumentSnapshot, updateDoc, getFirestore, onSnapshot } from "firebase/firestore"
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { Room } from "@/_types";
 import { useRouter } from "next/navigation";
+import { doc, DocumentReference, DocumentSnapshot, updateDoc, getFirestore, onSnapshot } from "firebase/firestore"
 import withAuth from "@/_hocs/withAuth";
 import useAuth from "@/_providers/auth/useAuth";
+import convertObjectToArray from "@/_utilities/convertObjectToArray";
+import { Direction } from "@/_enums";
+import {  Room, RoomRaw } from "@/_types";
 import Treasure from "@/_components/game/Treasure";
 import Pits from "@/_components/game/Pits";
-import { Direction } from "@/_enums";
+import PageContainer from "@/_components/PageContainer";
+
 
 const Page = ({ params: { id } }: { params: { id: string } }) => {
     const { user, isLoaded: isUserLoaded } = useAuth();
@@ -30,19 +33,30 @@ const Page = ({ params: { id } }: { params: { id: string } }) => {
         })
     }, [ docRef ]);
 
+    const setRoomData = useCallback((roomRaw: RoomRaw) => {
+        const room = {
+            ...roomRaw,
+            gamer1: {
+                ...roomRaw.gamer1,
+                pits: convertObjectToArray(roomRaw.gamer1.pits)
+            },
+            gamer2: {
+                ...roomRaw.gamer2,
+                pits: convertObjectToArray(roomRaw.gamer2.pits)
+            },
+        }
+        setRoom(room);
+    }, []);
+
     useEffect(() => {
         let timeoutId: NodeJS.Timeout;
 
         const unSubscribe = onSnapshot(docRef, (doc: DocumentSnapshot) => {
             timeoutId = setTimeout(() => setIsGameLoaded(true), 300);
 
-            if (doc.exists()) {
-                setRoom(doc.data() as Room);
-            } else {
-                push("/");
-            }
-        });
+            doc.exists() ? setRoomData(doc.data() as RoomRaw) : push("/")
 
+        });
         return () => {
             clearTimeout(timeoutId);
             unSubscribe();
@@ -61,39 +75,41 @@ const Page = ({ params: { id } }: { params: { id: string } }) => {
     if (!isGameLoaded) return null;
 
     return (
-        <main className="h-screen flex flex-col bg-no-repeat bg-cover bg-[url('/bg-room.jpg')]">
-            <div className="mt-20">
-                <div className="flex flex-col lg:flex-row items-center justify-center gap-5 p-5 lg:p-20">
-                    {room?.gamer1 && <Treasure treasure={room?.gamer1.treasure}/>}
+        <PageContainer>
+            <main className="h-screen flex flex-col bg-no-repeat bg-cover bg-[url('/bg-room.jpeg')]">
+                <div className="mt-20">
+                    <div className="flex flex-col lg:flex-row items-center justify-center gap-5 p-5">
+                        {room?.gamer1 && <Treasure treasure={room?.gamer1.treasure}/>}
 
-                    <div className="grid grid-cols-6 place-items-center gap-3">
-                        {room?.gamer1.pits && <Pits roomID={id}
-                                                    gamer={room.gamer1}
-                                                    rivalGamer={room.gamer2}
-                                                    gameOwner={room.gameOwner}
-                                                    moveOrder={room.moveOrder}
-                                                    isCurrentGamerPits={user?.uid == room.gamer1.id}
-                                                    isGameStarted={room.isGameStarted}
-                                                    isGameCompleted={room.isGameCompleted}
-                                                    position={Direction.Top}
-                        />
-                        }
-                        {room?.gamer2.pits && <Pits roomID={id}
-                                                    gamer={room.gamer2}
-                                                    rivalGamer={room.gamer1}
-                                                    gameOwner={room.gameOwner}
-                                                    moveOrder={room.moveOrder}
-                                                    isCurrentGamerPits={user?.uid == room.gamer2.id}
-                                                    isGameStarted={room.isGameStarted}
-                                                    isGameCompleted={room.isGameCompleted}
-                                                    position={Direction.Bottom}/>}
+                        <div className="grid grid-cols-6 place-items-center gap-3">
+                            {room?.gamer1.pits && <Pits roomID={id}
+                                                        gamer={room.gamer1}
+                                                        rivalGamer={room.gamer2}
+                                                        gameOwner={room.gameOwner}
+                                                        moveOrder={room.moveOrder}
+                                                        isCurrentGamerPits={user?.uid == room.gamer1.id}
+                                                        isGameStarted={room.isGameStarted}
+                                                        isGameCompleted={room.isGameCompleted}
+                                                        position={Direction.Top}
+                            /> }
+
+
+                            {room?.gamer2.pits && <Pits roomID={id}
+                                                        gamer={room.gamer2}
+                                                        rivalGamer={room.gamer1}
+                                                        gameOwner={room.gameOwner}
+                                                        moveOrder={room.moveOrder}
+                                                        isCurrentGamerPits={user?.uid == room.gamer2.id}
+                                                        isGameStarted={room.isGameStarted}
+                                                        isGameCompleted={room.isGameCompleted}
+                                                        position={Direction.Bottom}/>}
+                        </div>
+
+                        {room?.gamer2 && <Treasure treasure={room?.gamer2.treasure}/>}
                     </div>
-
-                    {room?.gamer2 && <Treasure treasure={room?.gamer2.treasure}/>}
                 </div>
-            </div>
-        </main>
-    );
+            </main>
+        </PageContainer>);
 };
 
 export default withAuth(Page)
