7cb8a7b9986f4b460a9eaf65071334950ea8e98d
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-10-08 22:49:24 +0300

    Memorize pit and stones

--- app/_components/game/Pit.tsx
+++ app/_components/game/Pit.tsx
@@ -1,84 +1,59 @@
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
+import {  AnimatePresence } from 'framer-motion';
+import { isEqual } from "lodash"
+import generatePositionsForStones from "@/_utilities/generatePositionsForStones";
+import { Pit as IPit, PositionThreshold } from "@/_types";
+import useDeepCompareMemoize from "@/_hooks/useDeepCompareMemoize";
+import getPitThresholdByWindowSize from "@/_utilities/getPitThresholdByWindowSize";
 import Stone from "./Stone";
-import { memo, useEffect, useState } from "react";
-import { generatePositionsForStones } from "@/_utilities";
-import { Position, PositionThreshold } from "@/_types";
 
 interface Props {
-    pit: number
+    pit: IPit,
     hasRight: boolean,
     onClick: () => void
 }
 
 
-function initialThreshold(): PositionThreshold {
-    if (window.innerWidth >= 1024) {  // This is just an example breakpoint for a tablet
-        return {
-            minLeft: 25,
-            minTop: 5,
-            maxLeft: 60,
-            maxTop: 60
-        }
-    } else if (window.innerWidth >= 768) {
-        return {
-            minLeft: 10,
-            minTop: 15,
-            maxLeft: 55,
-            maxTop: 50
-        }
-    } else if (window.innerWidth >= 640) {
-        return {
-            minLeft: 10,
-            minTop: 20,
-            maxLeft: 55,
-            maxTop: 50
-        }
-    } else {
-        return {
-            minLeft: 20,
-            minTop: 10,
-            maxLeft: 55,
-            maxTop: 60
-        }
-    }
-}
-
-
 const Pit = ({ pit, hasRight, onClick }: Props) => {
-    const [ positions, setPositions ] = useState<Position[]>([]);
     const className = hasRight ? "cursor-pointer bg-green-700/50 border-green-500" : "backdrop-blur-xl bg-white/25 border-white"
-    const [ currentThreshold, setCurrentThreshold ] = useState<PositionThreshold>(initialThreshold());
+    const [ currentThreshold, setCurrentThreshold ] = useState<PositionThreshold>(getPitThresholdByWindowSize());
+    const [ stonesWithPosition, setStonesWithPosition ] = useState(() => generatePositionsForStones(currentThreshold));
+    const memorizedPit = useDeepCompareMemoize<IPit>(pit);
 
-    useEffect(() => {
-        const handleResize = () => {
-            const newThreshold = initialThreshold();
+    const stones = useMemo(() => memorizedPit.map((stone) => ({
+        stone,
+        position: stonesWithPosition[stone.no - 1],
+    })), [ memorizedPit, stonesWithPosition ]);
 
-            if (JSON.stringify(newThreshold) === JSON.stringify(currentThreshold)) return;
-            setCurrentThreshold(initialThreshold());
+    const handleResize = useCallback(() => {
+        const newThreshold = getPitThresholdByWindowSize();
+
+        if (!isEqual(currentThreshold, newThreshold)) {
+            setCurrentThreshold(newThreshold);
+            setStonesWithPosition(generatePositionsForStones(newThreshold));
+
         }
-
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
     }, [ currentThreshold ]);
 
     useEffect(() => {
-        const usedPositions = generatePositionsForStones(pit, currentThreshold);
-        setPositions(usedPositions)
-    }, [ pit, currentThreshold ]);
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <div
             onClick={onClick}
             className={`${className} relative rounded-full w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 lg:w-32 lg:h-32 p-2 lg:p-4 border`}>
-            {positions.map((position, index) => (
-                <Stone key={index}
-                       hasRight={hasRight}
-                       position={position}/>
-            ))}
+            <AnimatePresence>
+                {stones.map(stone => (
+                    <Stone key={stone.stone.no} stone={stone} />
+                ))}
+            </AnimatePresence>
         </div>
     );
 };
+
 
 export default memo(Pit)
