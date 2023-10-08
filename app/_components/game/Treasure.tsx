9e9500cfa2b083aac9823a26efba42e0e882a611
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-10-08 22:53:36 +0300

    Memorize pit and stones

--- app/_components/game/Treasure.tsx
+++ app/_components/game/Treasure.tsx
@@ -1,57 +1,49 @@
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
+import { isEqual } from "lodash";
+import {  AnimatePresence } from 'framer-motion';
+import generatePositionsForStones from "@/_utilities/generatePositionsForStones";
+import useDeepCompareMemoize from "@/_hooks/useDeepCompareMemoize";
+import getTreasureThresholdByWindowSize from "@/_utilities/getTreasureThresholdByWindowSize";
+import { PositionThreshold, Stone as IStone } from "@/_types";
 import Stone from "./Stone";
-import { useEffect, useState } from "react";
-import { generatePositionsForStones } from "@/_utilities";
-import { Position, PositionThreshold } from "@/_types";
 
-function initialThreshold(): PositionThreshold {
-    if (window.innerWidth >= 1024) {  // This is just an example breakpoint for a tablet
-        return {
-            minLeft: 10,
-            minTop: 15,
-            maxLeft: 70,
-            maxTop: 80
-        }
-    } else {
-        return {
-            minLeft: 5,
-            minTop: 10,
-            maxLeft: 90,
-            maxTop: 70
-        }
-    }
-}
 
-export default function Treasure({ treasure }: { treasure: number }) {
-    const [ positions, setPositions ] = useState<Position[]>([]);
-    const [ currentThreshold, setCurrentThreshold ] = useState<PositionThreshold>(initialThreshold());
+
+const Treasure = ({ treasure }: { treasure: IStone[] }) => {
+    const [ currentThreshold, setCurrentThreshold ] = useState<PositionThreshold>(getTreasureThresholdByWindowSize());
+    const [ stonesWithPosition, setStonesWithPosition ] = useState(() => generatePositionsForStones(currentThreshold));
+    const memorizedTreasure = useDeepCompareMemoize<IStone[]>(treasure)
+
+    const stones = useMemo(() => memorizedTreasure.map((stone) => ({
+        stone,
+        position: stonesWithPosition[stone.no - 1]
+    })), [ memorizedTreasure, stonesWithPosition ]);
+
+    const handleResize = useCallback(() => {
+        const newThreshold = getTreasureThresholdByWindowSize();
+
+        if (!isEqual(currentThreshold, newThreshold)) {
+            setCurrentThreshold(newThreshold);
+            setStonesWithPosition(generatePositionsForStones(newThreshold));
+
+        }
+    }, [ currentThreshold ]);
 
     useEffect(() => {
-        const handleResize = () => {
-            const newThreshold = initialThreshold();
-
-            if (JSON.stringify(newThreshold) === JSON.stringify(currentThreshold)) return;
-            setCurrentThreshold(initialThreshold());
-        }
-
         window.addEventListener('resize', handleResize);
-
         return () => {
             window.removeEventListener('resize', handleResize);
         };
-    }, [currentThreshold]);
-
-    useEffect(() => {
-        const usedPositions = generatePositionsForStones(treasure, currentThreshold);
-        setPositions(usedPositions);
-
-    }, [ treasure, currentThreshold ]);
+    }, []);
 
     return (
         <div
             className="backdrop-lg p-8 h-20 sm:h-24 w-full lg:w-40 lg:h-96 grid grid-cols-3 gap-1 place-items-center rounded-[8rem] backdrop-blur-xl bg-white/25 border border-white">
-            {positions.map((position, index: number) => (
-                <Stone key={index} position={position}/>
-            ))}
+            <AnimatePresence>
+                {stones.map((stone) => <Stone stone={stone} key={stone.stone.no}/>)}
+            </AnimatePresence>
         </div>
     );
 };
+
+export default memo(Treasure)
