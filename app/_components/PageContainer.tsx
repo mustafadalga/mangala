1dfef510298270504f7f72ad5de75daf604fbe39
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-10-08 22:38:42 +0300

    Develop reusable page transition container

---
+++ app/_components/PageContainer.tsx
@@ -0,0 +1,21 @@
+import { motion } from 'framer-motion';
+import { ReactNode } from "react";
+
+const onTheRight = { x: '100%' }
+const inTheCenter = { x: 0 }
+const onTheLeft = { x: '-100%' }
+const transition = { duration: 0.6, ease: 'easeInOut' }
+
+export default function PageContainer({ children }: {
+    children: ReactNode
+}) {
+    return (
+        <motion.div
+            initial={onTheRight}
+            animate={inTheCenter}
+            exit={onTheLeft}
+            transition={transition}>
+            {children}
+        </motion.div>
+    );
+};
